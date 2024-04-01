d7f1784b1d495402f556d2724ec9e5320bb87dc1
SergEnergy <kis8883244@gmail.com>
2024-04-01 23:29:32 +0300

    feat: change greeting default param and update greeting text

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const greeting = (user = 'noName') => console.log(`Hello, aston trainee! I'm ${user}`);
+const greeting = (user = 'Ivan') => console.log(`Hello, aston trainee from Belarus! I'm ${user}`);
 
 greeting();
